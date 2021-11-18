614cd531bb0e1772217d7aeeba1b4d71cedd058d
Tin-Cam <mack679@hotmail.com.au>
2021-11-18 19:33:09 +1100

    Random stuff

--- player/Camera2D.gd
+++ player/Camera2D.gd
@@ -1,15 +1,21 @@
 extends Camera2D
 
 func _ready():
-	#_auto_set_limits()
+	_auto_set_limits()
 	pass
+	
+func _process(_delta):
+	if Input.is_action_just_pressed("zoom_in") :
+		set_zoom(get_zoom() / 2)
+	if Input.is_action_just_pressed("zoom_out") :
+		set_zoom(get_zoom() * 2)
 
 func _auto_set_limits():
 	limit_left = 0
 	limit_right = 0
 	limit_bottom = 0
 	
-	var tilemaps = get_tree().get_nodes_in_group("tile_map")
+	var tilemaps = get_tree().get_nodes_in_group("tile_maps")
 	for tilemap in tilemaps:
 		var used = tilemap.get_used_rect()
 		limit_left = min(used.position.x * tilemap.cell_size.x, limit_left)
